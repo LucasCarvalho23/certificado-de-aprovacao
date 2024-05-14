7ddf89dacacd013d88824e9bc07fa6cfeab24bf7
Lucas Carvalho <95986277+LucasCarvalho23@users.noreply.github.com>
2024-05-13 23:33:14 -0300

    recuperando dados do db

--- Private/php/autenticacao.php
+++ Private/php/autenticacao.php
@@ -1,5 +1,7 @@
 <?php 
 
+    session_start();
+
     require '../../Private/php/conexao.php';
     require '../../Private/php/turma.php';
     require '../../Private/php/consultas.php';
@@ -14,21 +16,42 @@
             $consultas = new Consultas($conexao, $turma);
             $resultado = $consultas->read($filtro);
 
-            if ($resultado) {
-                foreach ($resultado as $curso) {
-                    if ($curso['id_curso'] == $filtro) {
-                        echo $curso['nome'];
-                        echo (" ". $curso['nivel']); 
-                    }
+            $_SESSION['nome_curso'] = [];
 
-                    
-                    
-                }
+            if ($resultado) {
+                foreach ($resultado as $turma) {
+                    if ($turma['id_curso'] == $filtro && $turma['situacao'] == "aprovado") {
+                        $curso = $turma['id_curso'];
+                        $retorno = $consultas->readMore($curso);
+                        foreach ($retorno as $aluno) {
+                            $_SESSION['nome'][] = $aluno['nome'];
+                        }
+                    }
+                    $_SESSION['nome_curso'] = $retorno[0]['nome_curso'];
+                    $_SESSION['nivel_curso'] = $retorno[0]['nivel_curso'];
+                    $_SESSION['nome_empresa'] = $retorno[0]['nome_empresa'];
+                    $_SESSION['cnpj_empresa'] = $retorno[0]['cnpj_empresa'];
+                    $_SESSION['logradouro_empresa'] = $retorno[0]['logradouro_empresa'];
+                    $_SESSION['cnpj_numero'] = $retorno[0]['cnpj_numero'];
+                    $_SESSION['cnpj_complemento'] = $retorno[0]['cnpj_complemento'];
+                    $_SESSION['cnpj_bairro'] = $retorno[0]['cnpj_bairro'];
+                    $_SESSION['cnpj_cidade'] = $retorno[0]['cnpj_cidade'];
+                    $_SESSION['cnpj_estado'] = $retorno[0]['cnpj_estado'];
+                    $_SESSION['cnpj_cep'] = $retorno[0]['cnpj_cep'];
+                    $_SESSION['datainicio_curso'] = $retorno[0]['datainicio_curso'];
+                    $_SESSION['datafinal_curso'] = $retorno[0]['datafinal_curso'];
+                    $_SESSION['cargahoraria_curso'] = $retorno[0]['cargahoraria_curso'];
+                    $_SESSION['aptidao_curso'] = $retorno[0]['aptidao_curso'];
+                    break;      
+                }            
             } else {
                 echo "Nenhum curso encontrado com o ID especificado.";
             }
-            
+
+            header('Location: ../../Public/php/atestado.php');
+
         }
+            
     }
 
 ?>
--- Private/php/consultas.php
+++ Private/php/consultas.php
@@ -11,11 +11,42 @@
         }
 
         public function read($filtro) {
-            $query = 'select * from tb_curso where id_curso = :filtro';
+            $query = 'select * from tb_turma where id_curso = :filtro';
             $stmt = $this->conexao->prepare($query);
             $stmt->bindValue(':filtro', $filtro);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function readMore($curso) {
+            $curso = $curso;
+            $query = 'SELECT tb_aluno.nome, tb_aluno.documento, 
+                            tb_curso.nome as nome_curso, 
+                            tb_curso.nivel as nivel_curso,
+                            tb_empresa.nome as nome_empresa,
+                            tb_empresa.cnpj as cnpj_empresa,
+                            tb_empresa.logradouro as logradouro_empresa,
+                            tb_empresa.numero as cnpj_numero,
+                            tb_empresa.complemento as cnpj_complemento,
+                            tb_empresa.bairro as cnpj_bairro,
+                            tb_empresa.cidade as cnpj_cidade,
+                            tb_empresa.estado as cnpj_estado,
+                            tb_empresa.cep as cnpj_cep,
+                            tb_empresa.complemento as cnpj_complemento,
+                            tb_curso.datainicio as datainicio_curso,
+                            tb_curso.datafinal as datafinal_curso,
+                            tb_curso.cargahoraria as cargahoraria_curso,
+                            tb_curso.aptidao as aptidao_curso
+                    FROM tb_aluno 
+                    INNER JOIN tb_turma ON tb_aluno.id_aluno = tb_turma.id_aluno 
+                    INNER JOIN tb_curso ON tb_turma.id_curso = tb_curso.id_curso
+                    INNER JOIN tb_empresa ON tb_curso.id_empresa = tb_empresa.id_empresa
+                    WHERE tb_turma.id_curso = :curso AND tb_turma.situacao = "Aprovado"';
+            $stmt = $this->conexao->prepare($query);
+            $stmt->bindValue(':curso', $curso);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
         }
 
     }
